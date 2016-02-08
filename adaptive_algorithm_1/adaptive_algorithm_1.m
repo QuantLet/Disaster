1c935d75d7a4283eb0c8dca653440ec05df8ebd0
Tdannen <theo.dannenberg@live.de>
2016-02-09 00:22:51 +0100

    aligning assignment operator

--- adaptive_algorithm_1/adaptive_algorithm_1.m
+++ adaptive_algorithm_1/adaptive_algorithm_1.m
@@ -3,22 +3,22 @@ close all
 graph = 1;
 
 % Open & read txt file with names of files to work with
-filelist = 'filelist.txt';
-filenames = textread(filelist,'%s');
-nfiles = length(filenames);
+filelist    = 'filelist.txt';
+filenames   = textread(filelist,'%s');
+nfiles      = length(filenames);
 disp([filelist, ' has ', num2str(nfiles), ' files.']);
-figcount = 0;
+figcount    = 0;
 
 % Processing txt files
 for i = 1:nfiles
 
     % checking what kind of file (single or mapping) it is
-    filename = char(filenames(i));
+    filename    = char(filenames(i));
     fprintf('%s: single file\n', filename);
-    [x,y] = textread(filename,'%f%f','headerlines',1);     % Reads in txt file
-    data = [x y];
-    FS = process(data, graph, filename);
-    figcount = figcount + 1;
+    [x,y]       = textread(filename,'%f%f','headerlines',1);     % Reads in txt file
+    data        = [x y];
+    FS          = process(data, graph, filename);
+    figcount    = figcount + 1;
     % pausing program to look at figures
     if graph == 1 && figcount > 50
         fprintf('PAUSED after displaying %d figures\n\n', figcount);
@@ -34,17 +34,17 @@ end
 % Pre-processing data file
 function FS = process(data, graph, filename)
 
-data = sortrows(data,1);            % sort rows in ascending order
-rawdata = data;
-medfiltdata(:,2) = medfilt1(data(:,2),5);  % applies median filter to data
-data(:,2) = wden(medfiltdata(:,2), 'sqtwolog', 's', 'sln', 5, 'sym8'); %denoises spectra
+data                = sortrows(data,1);            % sort rows in ascending order
+rawdata             = data;
+medfiltdata(:,2)    = medfilt1(data(:,2),5);  % applies median filter to data
+data(:,2)           = wden(medfiltdata(:,2), 'sqtwolog', 's', 'sln', 5, 'sym8'); %denoises spectra
 
-residual = data(:,2) - rawdata(:,2);
-meanres = mean(residual);
-stdres = std(residual);
-cutoff = 3;
-badindex = find(abs(residual) > cutoff*stdres);
-goodindex = find(abs(residual) <= cutoff*stdres);
+residual    = data(:,2) - rawdata(:,2);
+meanres     = mean(residual);
+stdres      = std(residual);
+cutoff      = 3;
+badindex    = find(abs(residual) > cutoff*stdres);
+goodindex   = find(abs(residual) <= cutoff*stdres);
 figure(101);
 plot(data(:,1),residual(:),'b',data(badindex,1),residual(badindex),'ro'); hold on
 plot(data(:,1),-cutoff*stdres,'k',data(:,1),cutoff*stdres,'k')
@@ -57,16 +57,16 @@ close
 unique(chosen_order);
 order = FS(2);
 
-ysub_final = data(:,2) - yfit_final;
+ysub_final      = data(:,2) - yfit_final;
 ysub_final(ysub_final < 0) = 0;
-y_normalized = normalize(ysub_final);
-BGS_data = [data(:,1) y_normalized];
+y_normalized    = normalize(ysub_final);
+BGS_data        = [data(:,1) y_normalized];
 datastore(BGS_data, filename);                % saves to txt file
 
-ysub_final_weighted = data(:,2) - yfit_final_weighted;
+ysub_final_weighted     = data(:,2) - yfit_final_weighted;
 ysub_final_weighted(ysub_final_weighted < 0) = 0;
-y_normalized_weighted = normalize(ysub_final_weighted);
-BGS_data_weighted = [data(:,1) y_normalized_weighted];
+y_normalized_weighted   = normalize(ysub_final_weighted);
+BGS_data_weighted       = [data(:,1) y_normalized_weighted];
 
 if graph
 
@@ -101,11 +101,11 @@ initial_order = 4;
 initial_constraint = 0;
 [ysub_init yfit_init] = backsub_weighted(data,initial_order,initial_constraint);        % modified polynomial fit
 FSratio = (max(yfit_init)-min(yfit_init))/(max(ysub_init)-min(ysub_init));
-order = findorder(FSratio);
-FS = [FSratio order];
-YFITS = zeros(size(yfit_init,1),4);
-delta = 1;
-col = 0;
+order   = findorder(FSratio);
+FS      = [FSratio order];
+YFITS   = zeros(size(yfit_init,1),4);
+delta   = 1;
+col     = 0;
 for constraint = 0:1
     for z = order:delta:order+delta
         col = col + 1;
